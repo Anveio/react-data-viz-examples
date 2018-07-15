68f4165529d76a616ba73f9fc369d65e13e48d2b
anveio <im.shovonhasan@gmail.com>
2018-07-14 21:29:07 -0400

    Add BarChartDataPoint interface.

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,9 @@ export interface LineGraphDataPoint {
   readonly date: string;
   readonly price: number;
 }
+
+export interface BarChartDataPoint {
+  readonly name: string;
+  readonly uv: number;
+  readonly pv: number;
+}
