d02f1d2cc441f4963452c71df3f17f9ab19afc81
anveio <im.shovonhasan@gmail.com>
2018-07-15 22:03:19 -0400

    Add mock API calls for scatterplot data.

--- src/api/realtime.ts
+++ src/api/realtime.ts
@@ -1,5 +1,14 @@
-import { barChartData, lineGraphData } from './sample-data';
+import {
+  barChartData,
+  lineGraphData,
+  scatterPlotData01,
+  scatterPlotData02
+} from './sample-data';
 
 export const fetchLineGraphData = () => Promise.resolve(lineGraphData);
 
 export const fetchBarChartData = () => Promise.resolve(barChartData);
+
+export const fetchScatterPlotData01 = () => Promise.resolve(scatterPlotData01);
+
+export const fetchScatterPlotData02 = () => Promise.resolve(scatterPlotData02);
